1a315688a63e17da1b333050e1d2c2157685ca20
Suri <suri@svaahi.com>
2023-01-23 20:43:57 -0800

    Tutorial-2: Component Creation

---
+++ src/components/footer/footer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <footer>&copy; Suri's Lab Telugu</footer>;
+};
+
+export default Footer;
---
+++ src/components/header/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <h2>Todo Web App</h2>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
